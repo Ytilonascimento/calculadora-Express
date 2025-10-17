bca5e3554c7d7dd9b88f9e63c2c49f3ded934af9
Ytilonascimento <ytilo.nascimento2015@gmail.com>
2025-10-17 19:15:09 -0300

    calculadora que recebe os parâmetros via query

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/calculadora", (req, res) => {
+    try {
+        const { operacao } = req.query; // operação matemática (requisição)
+        const { numUm, numDois } = req.query;
+        let result = 0;
+        if (numUm == undefined || numUm == "" || isNaN(numUm) || numDois == undefined || numDois == "" || isNaN(numDois)) {
+            return res.status(400).send("Dado inválido")
+        }
+        switch (operacao) {
+            case "soma":
+                result = parseFloat(numUm) + parseFloat(numDois);
+                break;
 
+            case "subtracao":
+                result = parseFloat(numUm) - parseFloat(numDois);
+                break;
+
+            case "multiplicacao":
+                result = parseFloat(numUm) * parseFloat(numDois);
+                break;
+
+            case "divisao":
+                if (numDois===0);{
+                    return res.status(400).send("erro: Divisão por zero nao é permitidada");
+                }
+                result = parseFloat(numUm) / parseFloat(numDois);
+                break;
+
+            default:
+                return res.status(400).send("Dado inválido")
+
+        }
+
+        return res.status(200).send(`O resultado da operação é: ${result}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro interno no servidor");
+    }
+})
 
 
 // Inicia o servidor
